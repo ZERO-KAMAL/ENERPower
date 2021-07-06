fd0929590095de9f84d4338dae0ecf8d60687ad4
kamal khatri <59205492+ZERO-KAMAL@users.noreply.github.com>
2021-07-06 11:18:24 +0545

    last commit

--- Js/main.js
+++ Js/main.js
@@ -2,7 +2,9 @@ AOS.init();
 $(window).scroll(function () {
     if ($(this).scrollTop() > 150) {
         $('.arrow').show();
+        $('.navbar').addClass("fixed");
     } else {      
         $('.arrow').hide();
+        $('.navbar').removeClass("fixed");
     }
 });
